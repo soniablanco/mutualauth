defda9c1a427a4939593b20aa9e17601a4ba1ccc
Sonia <sonia@gmail.com>
2019-06-24 11:20:16 +0100

    requesting client cert

--- app/app.js
+++ app/app.js
@@ -1,13 +1,16 @@
 var fs = require('fs'); 
 var https = require('https'); 
 var options = { 
-    key: fs.readFileSync('/certificates/server-key.pem'), 
-    cert: fs.readFileSync('/certificates/server-crt.pem'), 
-    ca: fs.readFileSync('/certificates/ca-crt.pem'), 
+  key: fs.readFileSync('/certificates/server-key.pem'), 
+  cert: fs.readFileSync('/certificates/server-crt.pem'), 
+  ca: fs.readFileSync('/certificates/ca-crt.pem'), 
+    requestCert: true, 
+    rejectUnauthorized: true
 }; 
 https.createServer(options, function (req, res) { 
     console.log(new Date()+' '+ 
         req.connection.remoteAddress+' '+ 
+        req.socket.getPeerCertificate().subject.CN+' '+ 
         req.method+' '+req.url); 
     res.writeHead(200); 
     res.end("hello world\n"); 
